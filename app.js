2f023d780b61370104a3741b1e2613332b60e644
priyankbisht55 <135156326+priyankbisht55@users.noreply.github.com>
2024-08-20 14:30:36 +0530

    set up the postman

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
-const port = 3000;
-const tasks = import tasks from "./routes/tasks";
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config();
 
 //middleware
 app.use(express.json());
 
 //routes
 app.get("/hello", (req, res) => {
-    res.send("Task Manager App");
+  res.send("Task Manager App");
 });
 
-app.use('/api/v1/tasks', tasks);
+app.use("/api/v1/tasks", tasks);
 
 // app.get('/api/v1/tasks')    -get all the tasks
 // app.post('/api/v1/tasks')    -create a new task
-// app.get('/api/v1/tasks/:id')    -get a single task
+// app.get('/api/v1/tasks/:id')    -get a single tasks
 // app.patch('/api/v1/tasks/:id')   - update task
 // app.delete('/api/v1/tasks/:id')    - delete task
 
+const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}...`)
-});
-    
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+start();
