08e718396728d3acfafd68415107d73a0749cce4
priyankbisht55 <135156326+priyankbisht55@users.noreply.github.com>
2024-09-04 11:21:59 +0530

    almost final

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require('dotenv').config();
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json());
 
 //routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
 
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 
 // app.get('/api/v1/tasks')    -get all the tasks
 // app.post('/api/v1/tasks')    -create a new task
@@ -20,7 +23,7 @@ app.use("/api/v1/tasks", tasks);
 // app.patch('/api/v1/tasks/:id')   - update task
 // app.delete('/api/v1/tasks/:id')    - delete task
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
